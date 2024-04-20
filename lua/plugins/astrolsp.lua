3c13d849059a2572bea99162e0f5154aab446f6a
Bha-Gu <67630917+Bha-Gu@users.noreply.github.com>
2024-04-20 14:48:14 +0530

    Update astrolsp.lua

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -1,5 +1,5 @@
 if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
+-- TODO rust-analyzer
 -- AstroLSP allows you to customize the features in AstroNvim's LSP configuration engine
 -- Configuration documentation can be found with `:h astrolsp`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
